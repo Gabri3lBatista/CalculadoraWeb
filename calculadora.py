66949ad1f7121615253628b16bef71e3bd3ab50c
gabri3.batista@gmail.com <gabri3l.batist@gmail.com>
2022-08-09 19:57:11 -0500

    #2 Adicionar os operadores básicos

--- calculadora.py
+++ calculadora.py
@@ -6,16 +6,16 @@ print(number_1 + number_2)
 
 #subtracion 
 
-print('{} + {} = '.format(number_1, number_2))
+print('{} - {} = '.format(number_1, number_2))
 print(number_1 - number_2)
 
 #mutiplication 
 
-print('{} + {} = '.format(number_1, number_2))
+print('{} * {} = '.format(number_1, number_2))
 print(number_1 * number_2)
 
 #division 
 
-print('{} + {} = '.format(number_1, number_2))
+print('{} / {} = '.format(number_1, number_2))
 print(number_1 / number_2)
 
